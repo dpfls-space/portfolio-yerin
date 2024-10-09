104389d9fb25af143eedadf634b8e4d932cdfd01
dpfls-space <38265ye@gmail.com>
2024-10-09 16:24:47 +0900

    Exception 과 Advice 공통 개발

--- src/main/kotlin/com/yerin/portfolio/admin/exception/AdminException.kt
+++ src/main/kotlin/com/yerin/portfolio/admin/exception/AdminException.kt
@@ -2,9 +2,19 @@ package com.yerin.portfolio.admin.exception
 
 import org.springframework.http.HttpStatus
 
-class AdminException(
+abstract class AdminException(
     httpStatus: HttpStatus,
     message: String
 ) :RuntimeException(message) {
     val httpStatus: HttpStatus = httpStatus
 }
+
+class AdminBadRequestException(message: String) : AdminException(
+    httpStatus = HttpStatus.BAD_REQUEST,
+    message = message
+)
+
+class AdminInternalServerErrorException(message: String) : AdminException(
+    httpStatus = HttpStatus.INTERNAL_SERVER_ERROR,
+    message = message
+)
