2eccf9a94064d6e5ab679410ff538ca3e4cb4643
dpfls-space <38265ye@gmail.com>
2024-10-09 23:47:17 +0900

    삽입, 삭제 API 개발 - 연관관계 다대다

---
+++ src/main/kotlin/com/yerin/portfolio/admin/context/project/controller/AdminProjectSkillApiController.kt
@@ -0,0 +1,34 @@
+package com.yerin.portfolio.admin.context.project.controller
+
+import com.yerin.portfolio.admin.context.project.form.ProjectForm
+import com.yerin.portfolio.admin.context.project.form.ProjectSkillForm
+import com.yerin.portfolio.admin.context.project.service.AdminProjectService
+import com.yerin.portfolio.admin.context.project.service.AdminProjectSkillService
+import com.yerin.portfolio.admin.data.ApiResponse
+import com.yerin.portfolio.admin.data.TableDTO
+import com.yerin.portfolio.domain.entity.ProjectSkill
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/admin/api/projects/skills")
+class AdminProjectSkillApiController(
+    private val adminProjectSkillService: AdminProjectSkillService
+) {
+
+    @PostMapping
+    fun postProject(@RequestBody @Validated form: ProjectSkillForm): ResponseEntity<Any> {
+        adminProjectSkillService.save(form)
+
+        return ApiResponse.successCreate()
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteProjectSkill(@PathVariable id:Long) : ResponseEntity<Any> {
+        adminProjectSkillService.delete(id)
+
+        return ApiResponse.successDelete()
+    }
+
+}
