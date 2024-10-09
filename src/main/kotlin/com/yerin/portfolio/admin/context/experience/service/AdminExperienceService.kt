e431a93be383a3b05ef0dee643e56306c650fbe6
dpfls-space <38265ye@gmail.com>
2024-10-09 23:31:06 +0900

    삽입, 수정, 조회 API 개발 - 연관관계 일대다

--- src/main/kotlin/com/yerin/portfolio/admin/context/experience/service/AdminExperienceService.kt
+++ src/main/kotlin/com/yerin/portfolio/admin/context/experience/service/AdminExperienceService.kt
@@ -1,11 +1,14 @@
 package com.yerin.portfolio.admin.context.experience.service
 
+import com.yerin.portfolio.admin.context.experience.form.ExperienceForm
 import com.yerin.portfolio.admin.data.TableDTO
 import com.yerin.portfolio.admin.exception.AdminBadRequestException
 import com.yerin.portfolio.domain.entity.Experience
 import com.yerin.portfolio.domain.entity.ExperienceDetail
 import com.yerin.portfolio.domain.repository.ExperienceRepository
+import org.aspectj.weaver.model.AsmRelationshipUtils
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class AdminExperienceService (
@@ -22,9 +25,52 @@ class AdminExperienceService (
     fun getExperienceDetailTable(id:Long?): TableDTO {
         val classInfo = ExperienceDetail::class
         val entities = if (id != null) experienceRepository.findById(id)
-            .orElseThrow { throw AdminBadRequestException("ID ${id} 에 해당하는 데이터를 찾을 수가 없습니다.")}
+            .orElseThrow { throw AdminBadRequestException("ID ${id} 에 해당하는 데이터를 찾을 수가 없습니다.") }
             .details else emptyList()
 
         return TableDTO.from(classInfo, entities)
     }
+
+    @Transactional
+    fun save(form: ExperienceForm) {
+        val experienceDetails = form.details
+            ?.map { detail -> detail.toEntity() }
+            ?.toMutableList()
+
+        val experience = form.toEntity()
+        experience.addDetails(experienceDetails)
+
+        experienceRepository.save(experience)
+    }
+
+    @Transactional
+    fun update(id:Long, form:ExperienceForm) {
+        val experience = experienceRepository.findById(id)
+            .orElseThrow { throw AdminBadRequestException("ID ${id} 에 해당하는 데이터를 찾을 수가 없습니다.") }
+
+        experience.update(
+            title = form.title,
+            description = form.description,
+            startYear = form.startYear,
+            startMonth = form.startMonth,
+            endYear = form.endYear,
+            endMonth = form.endMonth,
+            isActive = form.isActive
+        )
+
+        val detailMap = experience.details.map { it.id to it }.toMap()
+        form.details?.forEach {
+            val entity = detailMap.get(it.id)
+            if (entity != null) {
+                entity.update(
+                    content = it.content,
+                    isActive = it.isActive
+                )
+            } else {
+                experience.details.add(it.toEntity())
+            }
+        }
+
+    }
+
 }
