42568410905ca7af644e9c1efc0dd36321ad2a16
dpfls-space <38265ye@gmail.com>
2024-10-10 23:42:08 +0900

    ProjectSkill 수정

--- src/main/kotlin/com/yerin/portfolio/domain/entity/ProjectSkill.kt
+++ src/main/kotlin/com/yerin/portfolio/domain/entity/ProjectSkill.kt
@@ -3,7 +3,7 @@ package com.yerin.portfolio.domain.entity
 import jakarta.persistence.*
 
 @Entity
-class ProjectSkill(project: Project, skill: Skill): BaseEntity() {
+class ProjectSkill(project: Project, skill: Skill) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,9 +12,9 @@ class ProjectSkill(project: Project, skill: Skill): BaseEntity() {
 
     @ManyToOne(targetEntity = Project::class, fetch = FetchType.LAZY)
     @JoinColumn(name = "project_id", nullable = false)
-    var project : Project = project
+    var project: Project = project
 
     @ManyToOne(targetEntity = Skill::class, fetch = FetchType.LAZY)
     @JoinColumn(name = "skill_id", nullable = false)
-    var skill : Skill = skill
+    var skill: Skill = skill
 }
